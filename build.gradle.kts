968333673c3ef626e745e7091a7017abb45287e1
MarianKijewski <mariankijewski@outlook.com>
2025-07-16 10:21:55 +0200

    bump SDK version from 0.0.12 to 0.0.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.wire", "wire-apps-jvm-sdk", "0.0.12")
+    implementation("com.wire", "wire-apps-jvm-sdk", "0.0.13")
     // stdlib
     implementation(kotlin("stdlib-jdk8"))
     // extension functions
