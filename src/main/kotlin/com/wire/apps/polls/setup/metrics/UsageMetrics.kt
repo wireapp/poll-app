be33207e3b1bfcc3cb24bff927bee35c129e0f15
baris.alis <baris.alis@wire.com>
2025-11-26 10:38:09 +0100

    Revert "- Add "delete poll" custom metric."
    This reverts commit a1b9a541cdb6845ef0904e39d188a4e112e3b89b.

--- src/main/kotlin/com/wire/apps/polls/setup/metrics/UsageMetrics.kt
+++ src/main/kotlin/com/wire/apps/polls/setup/metrics/UsageMetrics.kt
@@ -16,20 +16,11 @@ class UsageMetrics(
         .description("Number of Poll creation commands received")
         .register(registry)
 
-    private val deletePollCommandCounter: Counter = Counter
-        .builder("pollapp_delete_poll_commands_total")
-        .description("Number of Poll deletion commands received")
-        .register(registry)
-
     fun onHelpCommand() {
         helpCommandCounter.increment()
     }
 
     fun onCreatePollCommand() {
         createPollCommandCounter.increment()
-    }
-
-    fun onDeletePollCommand() {
-        deletePollCommandCounter.increment()
     }
 }
