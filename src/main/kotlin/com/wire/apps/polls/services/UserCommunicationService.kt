5c55e23e050a10cd1d819f2555ffac694df140de
MarianKijewski <mariankijewski@outlook.com>
2026-01-17 20:05:59 +0100

    apply common welcome message structure

--- src/main/kotlin/com/wire/apps/polls/services/UserCommunicationService.kt
+++ src/main/kotlin/com/wire/apps/polls/services/UserCommunicationService.kt
@@ -22,13 +22,12 @@ class UserCommunicationService(
     private val version: String
 ) {
     private companion object : KLogging() {
+        const val WELCOME_TEXT =
+            "👋 Hi, I'm the Poll App. Thanks for adding me to the conversation.\n" +
+                "You can use me to create polls directly in Wire.\n" +
+                "I'm here to help make everyday work a little easier."
         const val USAGE = "To create poll please text: " +
             "`/poll \"Question\" \"Option 1\" \"Option 2\"`. To display usage write `/poll help`"
-        val commands = """
-            Following commands are available:
-            `/poll "Question" "Option 1" "Option 2"` will create poll
-            `/poll help` to show help
-        """.trimIndent()
     }
 
     /**
@@ -38,7 +37,12 @@ class UserCommunicationService(
         manager: WireApplicationManager,
         conversationId: QualifiedId
     ) {
-        manager.send(textMessage(conversationId, "Hello, I'm Poll App. $USAGE"))
+        val message = buildString {
+            appendLine(WELCOME_TEXT)
+            append(USAGE)
+        }
+
+        manager.send(textMessage(conversationId, message))
         logger.info("App sent a welcome message. conversationId: $conversationId")
     }
 
@@ -69,7 +73,7 @@ class UserCommunicationService(
         manager: WireApplicationManager,
         conversationId: QualifiedId
     ) {
-        manager.send(textMessage(conversationId, commands))
+        manager.send(textMessage(conversationId, USAGE))
     }
 
     /**
